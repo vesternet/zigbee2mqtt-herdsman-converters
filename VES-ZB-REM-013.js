8d3904a34bc6a314a3861c45bc833e9ea4004ac5
martyn-vesternet <74303058+martyn-vesternet@users.noreply.github.com>
2024-01-30 11:05:44 +0000

    fix battery level reporting
    earlier versions of these devices report 0 - 100 for battery level, so no conversion was required.
    later versions of these devices report 0 - 200 for battery level to allow for higher precision, so conversion is required.

--- VES-ZB-REM-013.js
+++ VES-ZB-REM-013.js
@@ -3,16 +3,42 @@ const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const extend = require('zigbee-herdsman-converters/lib/extend');
+const utils = require('zigbee-herdsman-converters/lib/utils');
 const e = exposes.presets;
 const ea = exposes.access;
 
+
+const vesternet = { 
+    fz: {
+        battery: {
+            cluster: 'genPowerCfg',
+            type: ['attributeReport', 'readResponse'],
+            convert: (model, msg, publish, options, meta) => {
+                const payload = {};
+                if (msg.data.hasOwnProperty('batteryPercentageRemaining') && (msg.data['batteryPercentageRemaining'] < 255)) {
+                    //older firmware versions reported 0 - 100 so don't need percentage dividing
+                    //newer firmware versions report 0 - 200 so do need percentage dividing
+                    let dontDividePercentage = true;
+                    if (meta.device.softwareBuildID == "2.7.6_r25") {
+                        dontDividePercentage = false;
+                    }
+                    let percentage = msg.data['batteryPercentageRemaining'];
+                    percentage = dontDividePercentage ? percentage : percentage / 2;
+                    payload.battery = utils.precisionRound(percentage, 2);
+                }
+
+                return payload;
+            },
+        }
+    },
+};
 
 const definition = {
     fingerprint: [{modelID: 'ZGRC-KEY-013', softwareBuildID: '2.5.3_r20'}, {modelID: 'ZGRC-KEY-013', softwareBuildID: '2.7.6_r25'}],
     model: 'VES-ZB-REM-013',
     vendor: 'Vesternet',
     description: 'Zigbee remote control - 12 button',
-    fromZigbee: [fz.command_on, fz.command_off, fz.command_move, fz.command_stop, fz.command_recall, fz.battery, fz.ignore_genOta],
+    fromZigbee: [fz.command_on, fz.command_off, fz.command_move, fz.command_stop, fz.command_recall, vesternet.fz.battery, fz.ignore_genOta],
     exposes: [e.battery(),
         e.action(['on_*', 'off_*', 'stop_*', 'brightness_move_up_*', 'brightness_move_down_*', 'brightness_stop_*', 'recall_*'])],
     toZigbee: [],
--- VES-ZB-WAL-006.js
+++ VES-ZB-WAL-006.js
@@ -3,16 +3,42 @@ const tz = require('zigbee-herdsman-converters/converters/toZigbee');
 const exposes = require('zigbee-herdsman-converters/lib/exposes');
 const reporting = require('zigbee-herdsman-converters/lib/reporting');
 const extend = require('zigbee-herdsman-converters/lib/extend');
+const utils = require('zigbee-herdsman-converters/lib/utils');
 const e = exposes.presets;
 const ea = exposes.access;
 
+
+const vesternet = { 
+    fz: {
+        battery: {
+            cluster: 'genPowerCfg',
+            type: ['attributeReport', 'readResponse'],
+            convert: (model, msg, publish, options, meta) => {
+                const payload = {};
+                if (msg.data.hasOwnProperty('batteryPercentageRemaining') && (msg.data['batteryPercentageRemaining'] < 255)) {
+                    //older firmware versions reported 0 - 100 so don't need percentage dividing
+                    //newer firmware versions report 0 - 200 so do need percentage dividing
+                    let dontDividePercentage = true;
+                    if (meta.device.softwareBuildID == "2.7.6_r25") {
+                        dontDividePercentage = false;
+                    }
+                    let percentage = msg.data['batteryPercentageRemaining'];
+                    percentage = dontDividePercentage ? percentage : percentage / 2;
+                    payload.battery = utils.precisionRound(percentage, 2);
+                }
+
+                return payload;
+            },
+        }
+    },
+};
 
 const definition = {
     fingerprint: [{modelID: 'ZG2833K2_EU07', softwareBuildID: '2.5.3_r20'}, {modelID: 'ZG2833K2_EU07', softwareBuildID: '2.7.6_r25'}],
     model: 'VES-ZB-WAL-006',
     vendor: 'Vesternet',
     description: 'Zigbee wall controller - 2 button',
-    fromZigbee: [fz.command_on, fz.command_off, fz.command_move, fz.command_stop, fz.battery, fz.ignore_genOta],
+    fromZigbee: [fz.command_on, fz.command_off, fz.command_move, fz.command_stop, vesternet.fz.battery, fz.ignore_genOta],
     exposes: [e.battery(), e.action([
         'on_1', 'off_1', 'stop_1', 'brightness_move_up_1', 'brightness_move_down_1', 'brightness_stop_1'])],
     toZigbee: [],
