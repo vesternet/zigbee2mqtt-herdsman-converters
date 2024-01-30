35bb7ba91772da99915e333c2a1087bbad8ecb07
martyn-vesternet <74303058+martyn-vesternet@users.noreply.github.com>
2024-01-30 12:32:11 +0000

    adds support for VES-ZB-TEM-27
    add support for the VES-ZB-TEM-27 Temperature & Humidity Sensor.
    
    exposes temperature, humidity, battery percentage and linkquality.

---
+++ VES-ZB-TEM-27.js
@@ -0,0 +1,28 @@
+const fz = require('zigbee-herdsman-converters/converters/fromZigbee');
+const tz = require('zigbee-herdsman-converters/converters/toZigbee');
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const reporting = require('zigbee-herdsman-converters/lib/reporting');
+const extend = require('zigbee-herdsman-converters/lib/extend');
+const e = exposes.presets;
+const ea = exposes.access;
+
+
+const definition = {
+    fingerprint: [{modelID: 'TempAndHumSensor-ZB3.0', softwareBuildID: '2.01'}, {modelID: 'TempAndHumSensor-ZB3.0', softwareBuildID: '2.1'}, {modelID: 'TempAndHumSensor-ZB3.0', softwareBuildID: '2.04'}],
+    model: 'VES-ZB-TEM-27',
+    vendor: 'Vesternet',
+    description: 'Zigbee temperature and humidity sensor',
+    fromZigbee: [fz.temperature, fz.humidity, fz.battery],
+    exposes: [e.temperature(), e.humidity(), e.battery()],
+    toZigbee: [],
+    whiteLabel: [{vendor: 'HZC Electric', model: 'S093TH-ZG'}],
+    configure: async (device, coordinatorEndpoint, logger) => {
+        const endpoint = device.getEndpoint(1);
+        await reporting.bind(endpoint, coordinatorEndpoint, ['msTemperatureMeasurement', 'msRelativeHumidity', 'genPowerCfg']);
+        await reporting.temperature(endpoint, {min: 60, max: 1800, change: 100});
+        await reporting.humidity(endpoint, {min: 60, max: 1800, change: 100});
+        await reporting.batteryPercentageRemaining(endpoint, {min: 3600, max: 7200});        
+    },
+};
+
+module.exports = definition;
